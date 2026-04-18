bc834e7b81ef324dc04f8f769e663fa40d54cde2
Nikhil Thakur <nikhil2050thakur2001@gmail.com>
2026-04-18 21:22:28 +0530

    feat: scaffold cli package

---
+++ cli/src/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { runInit } from './init.js';
+
+const [,, command, ...args] = process.argv;
+const force = args.includes('--force');
+
+if (command === 'init') {
+  runInit({ force }).catch(err => {
+    console.error(err.message);
+    process.exit(1);
+  });
+} else {
+  console.log('Usage: ai-omega init [--force]');
+  process.exit(1);
+}
